002c368006fd2ca638e256d7075270068e402b5f
nfarabullini <nicoletta.farabullni@mirai-solutions.com>
2021-12-07 22:05:14 +0100

    updates to candidate set for Lb Keough

--- DTW_lb_rank.py
+++ DTW_lb_rank.py
@@ -2,76 +2,81 @@ from common_funs import compute_df_query, compute_files_ls, dtw_horizontal
 from lb_funs import calc_min_dist_MD_filtered, upper_envelope, lower_envelope, construct_lower_MBRs, construct_upper_MBRs
 
 import numpy as np
-import pandas as pd
-import os
-import json
 import warnings
 import time
-from scipy.cluster.hierarchy import dendrogram, linkage
-from scipy.spatial.distance import squareform
-from sklearn.cluster import AgglomerativeClustering
-import matplotlib.pyplot as plt
+
 warnings.filterwarnings('ignore')
-from metrics_msproject import compute_angle_vector
-
-sakoe_chiba = 20
-N = 15
+# initial values to be set by the user
 th = 30
-
-path = "./files_dances"
-# group files belonging to each video in a different sublist, combine all sublist into one list
-files_ls = compute_files_ls(path)
-dendro_arr_fill = np.zeros((52, 52))
-start = time.time()
-ls_lb_files = []
 index_query = 5
 
-# compute angle vectors for query video
-newDF_query = compute_df_query(path, files_ls, index_query)
+# start up values for computation
+tot_time_current = np.inf
+sc_N_comb = []
 
-u = upper_envelope(newDF_query, sakoe_chiba)
-l = lower_envelope(newDF_query, sakoe_chiba)
+path = "./files_dances"
+for sakoe_chiba in range(1, 21):
+    for N in range(1, 21):
+        start = time.time()
+        # group files belonging to each video in a different sublist, combine all sublist into one list
+        files_ls = compute_files_ls(path)
+        #dendro_arr_fill = np.zeros((52, 52))
+        ls_lb_files = []
 
-Q_u_r = construct_upper_MBRs(u, N)
-Q_l_r = construct_lower_MBRs(l, N)
+        # compute angle vectors for query video
+        newDF_query = compute_df_query(path, files_ls, index_query)
 
-# loop over all other videos to be compared with query
-for g in range(0, 52):
-    if g != index_query:
-        newDF = compute_df_query(path, files_ls, g)
+        u = upper_envelope(newDF_query, sakoe_chiba)
+        l = lower_envelope(newDF_query, sakoe_chiba)
 
-        T_u_r = construct_upper_MBRs(newDF, N)
-        T_l_r = construct_lower_MBRs(newDF, N)
+        Q_u_r = construct_upper_MBRs(u, N)
+        Q_l_r = construct_lower_MBRs(l, N)
 
-        # compute LB Keough similarity
-        lb1 = calc_min_dist_MD_filtered(T_u_r, T_l_r, Q_u_r, Q_l_r, N, th)
-        dendro_arr_fill[index_query, g] = lb1
-        # Maybe 50 instead of 30
-        if lb1 < th:
-            ls_lb_files.append([g, newDF, lb1])
-        print(g)
-#print(index_query)
+        # loop over all other videos to be compared with query
+        for g in range(0, 52):
+            if g != index_query:
+                newDF = compute_df_query(path, files_ls, g)
 
+                T_u_r = construct_upper_MBRs(newDF, N)
+                T_l_r = construct_lower_MBRs(newDF, N)
 
-#Display LB Keough ranking
-ls_lb_sorted = sorted(ls_lb_files, key=lambda x: x[2])
-for i in range(len(ls_lb_sorted)):
-    file_index = ls_lb_sorted[i][0]
-    print(files_ls[file_index][0])
+                # compute LB Keough similarity
+                lb1 = calc_min_dist_MD_filtered(T_u_r, T_l_r, Q_u_r, Q_l_r, N, th)
+                #dendro_arr_fill[index_query, g] = lb1
+                # Maybe 50 instead of 30
+                if lb1 < th:
+                    ls_lb_files.append([g, newDF, lb1])
+                print(g)
+        #print(index_query)
 
-#Calculate accurate DTW
-actual_dtw_ls = []
-for i in range(len(ls_lb_files)):
-    newDF = ls_lb_files[i][1]
-    actual_dtw = dtw_horizontal(newDF, newDF_query)
-    actual_dtw_ls.append([ls_lb_files[i][0], actual_dtw])
-    print(i)
-actual_dtw_sorted = sorted(actual_dtw_ls, key=lambda x: x[1])
+        # Calculate accurate DTW
+        actual_dtw_ls = []
+        for i in range(len(ls_lb_files)):
+            newDF = ls_lb_files[i][1]
+            actual_dtw = dtw_horizontal(newDF, newDF_query)
+            if actual_dtw < th:
+                actual_dtw_ls.append([ls_lb_files[i][0], actual_dtw])
+            print(i)
 
-#Display DTW ranking
-for i in range(len(actual_dtw_sorted)):
-    file_index = actual_dtw_sorted[i][0]
-    print(files_ls[file_index][0])
-files_ls[index_query][0]
+        end = time.time()
+        tot_time = end - start
+        if tot_time < tot_time_current:
+            tot_time_current = tot_time
+            sc_N_comb = [sakoe_chiba, N]
+
+print(sc_N_comb, tot_time_current)
+# #Display LB Keough ranking
+# ls_lb_sorted = sorted(ls_lb_files, key=lambda x: x[2])
+# for i in range(len(ls_lb_sorted)):
+#     file_index = ls_lb_sorted[i][0]
+#     print(files_ls[file_index][0])
+
+#actual_dtw_sorted = sorted(actual_dtw_ls, key=lambda x: x[1])
+
+# #Display DTW ranking
+# for i in range(len(actual_dtw_sorted)):
+#     file_index = actual_dtw_sorted[i][0]
+#     print(files_ls[file_index][0])
+# files_ls[index_query][0]
 
 
