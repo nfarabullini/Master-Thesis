7f15b300fbcc6c146d281f39af7c57e7af7a47a3
nfarabullini <nicoletta.farabullni@mirai-solutions.com>
2022-02-16 17:49:19 +0100

    Updates on timing plots

--- query_files/build_plots.py
+++ query_files/build_plots.py
@@ -8,23 +8,23 @@ read_file = pd.read_csv (r'DTW_lb_times.csv')
 fig = plt.figure()
 ax = plt.axes(projection ='3d')
 ax.scatter3D(read_file["Sakoe_Chiba"], read_file["N"], read_file["Time (s)"])
-plt.savefig('./3D_MBRs_sc.png', format='png', bbox_inches='tight')
+plt.savefig('./plots/3D_MBRs_sc.png', format='png', bbox_inches='tight')
 
 # 2D plots
-sc_1 = 10
-sc_2 = 15
+sc_1 = 7
+sc_2 = 13
 sc_3 = 19
-sc_4 = 5
+sc_4 = 3
 plt.figure()
-plt.plot(read_file["N"][read_file["Sakoe_Chiba"].isin([sc_4])], read_file["Time (s)"][read_file['Sakoe_Chiba'].isin([sc_4])], label = "Sakoe-Chiba = 5")
-plt.plot(read_file["N"][read_file["Sakoe_Chiba"].isin([sc_1])], read_file["Time (s)"][read_file['Sakoe_Chiba'].isin([sc_1])], label = "Sakoe-Chiba = 10")
-plt.plot(read_file["N"][read_file["Sakoe_Chiba"].isin([sc_2])], read_file["Time (s)"][read_file['Sakoe_Chiba'].isin([sc_2])], label = "Sakoe-Chiba = 15")
+plt.plot(read_file["N"][read_file["Sakoe_Chiba"].isin([sc_4])], read_file["Time (s)"][read_file['Sakoe_Chiba'].isin([sc_4])], label = "Sakoe-Chiba = 3")
+plt.plot(read_file["N"][read_file["Sakoe_Chiba"].isin([sc_1])], read_file["Time (s)"][read_file['Sakoe_Chiba'].isin([sc_1])], label = "Sakoe-Chiba = 7")
+plt.plot(read_file["N"][read_file["Sakoe_Chiba"].isin([sc_2])], read_file["Time (s)"][read_file['Sakoe_Chiba'].isin([sc_2])], label = "Sakoe-Chiba = 13")
 plt.plot(read_file["N"][read_file["Sakoe_Chiba"].isin([sc_3])], read_file["Time (s)"][read_file['Sakoe_Chiba'].isin([sc_3])], label = "Sakoe-Chiba = 19")
 plt.legend()
 plt.ylabel('Time (s)')
 plt.xlabel('MBRs')
 plt.title("Time VS MBRs")
-plt.savefig('./sc_changing_MBRs.png', format='png', bbox_inches='tight')
+plt.savefig('./plots/MBRs_with_set_sc.png', format='png', bbox_inches='tight')
 
 N_1 = 10
 N_2 = 15
@@ -39,4 +39,4 @@ plt.legend()
 plt.ylabel('Time (s)')
 plt.xlabel('Sakoe-Chiba lengths')
 plt.title("Time VS Sakoe-Chiba lengths")
-plt.savefig('./MBRs_changing_sc.png', format='png', bbox_inches='tight')
+plt.savefig('./plots/sc_with_set_MBRs.png', format='png', bbox_inches='tight')
