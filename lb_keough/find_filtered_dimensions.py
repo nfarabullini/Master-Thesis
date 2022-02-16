6631925a992cff9e6225e4ff367c1124d5f77c72
nfarabullini <nicoletta.farabullni@mirai-solutions.com>
2022-02-16 12:11:41 +0100

    updates to LB Keough dendro and filtered dimensions

--- lb_keough/find_filtered_dimensions.py
+++ lb_keough/find_filtered_dimensions.py
@@ -2,17 +2,12 @@
 
 from common_funs import compute_df, compute_files_ls
 from lb_funs import calc_min_dist_MD, upper_envelope, lower_envelope, construct_lower_MBRs, construct_upper_MBRs
+from lb_funs import match_clustering_groups
 
 import numpy as np
-import pandas as pd
 import warnings
-import time
-import os
 
-from scipy.cluster.hierarchy import dendrogram, linkage
-from scipy.spatial.distance import squareform
 from sklearn.cluster import AgglomerativeClustering
-import matplotlib.pyplot as plt
 
 warnings.filterwarnings('ignore')
 
@@ -25,7 +20,6 @@ sakoe_chiba = 3
 # group files belonging to each video in a different sublist, combine all sublist into one list
 files_ls = compute_files_ls(path)
 dendro_arr_fill = np.zeros((n_videos, n_videos))
-start = time.time()
 for index_query in range(0, n_videos):
     # compute angle vectors for query video
     newDF_query = compute_df(path, files_ls, index_query)
@@ -47,18 +41,18 @@ for index_query in range(0, n_videos):
         dendro_arr_fill[index_query, g] = lb1
     print(index_query)
 
-end = time.time()
-print(end - start)
+ground_truth = [
+            ['BS_F_BK_', 'BS_F_LT_', 'BS_F_RT_', 'BS_S_BK_', 'BS_S_FT_', 'BS_S_LT_', 'BS_S_RT_', 'SS_F_BK_', 'SS_F_FT_',
+             'SS_F_LT_', 'SS_F_RT_', 'SS_S_BK_', 'SS_S_LT_', 'SS_S_RT_', 'SYN_R_', 'SYN_U_'],
+            ['LD_F_dis_', 'LD_F_small_', 'LD_S_dis_', 'LD_S_small_', 'LU_F_dis_', 'LU_S_dis_'],
+            ['BJ_FT_', 'SJ_FT_'],
+            ['AR_', 'TA_', 'LU_F_big_', 'LU_S_big_'],
+            ['BJ_RT_', 'SJ_RT_', 'BJ_LT_', 'SJ_LT_'],
+            ['BBS_F_BK_', 'BBS_F_FT_', 'BBS_S_BK_', 'BBS_S_FT_', 'BSS_S_BK_', 'BSS_S_FT_'],
+            ['TB_F_FB_', 'TB_S_', 'TB_S_FB_', 'TF_F_', 'TF_S_', 'TL_F_', 'TL_S_', 'TOS_F_', 'TOS_S_', 'TR_F_', 'TR_S_'],
+            ['SYN_K_'],
+            ['BJ_BK_', 'SJ_BK_']]
 
-dendro_arr_complete = dendro_arr_fill + dendro_arr_fill.T - np.diag(np.diag(dendro_arr_fill))
-pd.DataFrame(dendro_arr_complete).to_csv("LB_sim_arr_horizontal_filtered_csv")
-clustering = AgglomerativeClustering(n_clusters = n_clusters, affinity = 'precomputed', linkage = 'average').fit(dendro_arr_complete)
-file1 = open("LB_sim_arr_horizontal_filtered.txt", "w")
-txt_time = "time taken for simulation " + str(end - start)
-file1.writelines(str(clustering.labels_) + txt_time)
-file1.close()
-
-# create dendrogram
 files_names = []
 for i in range(0, n_videos):
     cont = False
@@ -71,9 +65,7 @@ for i in range(0, n_videos):
     if cont:
         continue
 
-dists = squareform(dendro_arr_complete)
-Z = linkage(dists, 'average')
-plt.figure()
-dn = dendrogram(Z, labels = files_names)
-os.path.abspath('/Dendrograms/')
-plt.savefig('./LB_dendro_horizontal_filtered.png', format='png', bbox_inches='tight')
+dendro_arr_complete = dendro_arr_fill + dendro_arr_fill.T - np.diag(np.diag(dendro_arr_fill))
+clustering = AgglomerativeClustering(n_clusters = n_clusters, affinity = 'precomputed', linkage = 'average').fit(dendro_arr_complete)
+number_matches = match_clustering_groups(ground_truth, files_names, clustering.labels_, 9)
+print(number_matches)
